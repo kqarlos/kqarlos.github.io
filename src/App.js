49c783b07b792fe13128f3a6ff94089c1a11471c
kqarlos <kqarlos@hotmail.com>
2021-07-12 03:48:38 -0700

    Adds back to top button

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import About from './components/About'
 import NavBar from './components/NavBar'
 import Portfolio from './components/Portfolio'
 import Footer from './components/Footer'
+import TopButton from './components/TopButton';
 
 function App() {
   return (
     <div>
-
+      
+      <TopButton />
       <About />
       <NavBar />
       <Portfolio />
---
+++ src/components/TopButton/index.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+import './style.css';
+
+function TopButton() {
+
+    function handleScroll() {
+        if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+            document.getElementById("topBtn").style.display = 'block'
+        } else {
+            document.getElementById("topBtn").style.display = 'none'
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+    }, []);
+
+
+    return (
+
+        <a href="#about" class="btn text-white bg-info contentText smoothLink" id="topBtn"><span id="t">Top </span><i
+            class="fas fa-arrow-circle-up"></i></a>
+    )
+}
+
+export default TopButton;
