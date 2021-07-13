cbe7013f7b0c141dc345cd2f93fe2b41ce94387f
kqarlos <kqarlos@hotmail.com>
2021-07-13 10:06:37 -0700

    Adds project infor to global state. Refactors project component

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -11,7 +11,7 @@ function Project() {
     return (
 
 
-        <div id="project" data-project="{{project}}" style="display:{{projectDisplay}};">
+        <div id="project" data-project={state.currentProject} style={{ display: state.projectDisplay }}>
 
             {/* <!-- start grid --> */}
 
@@ -26,7 +26,7 @@ function Project() {
 
                 <div class="row">
                     <div class="col-12 h4 mb-2">Description</div>
-                    <div class="col-12">{{ description }}</div>
+                    <div class="col-12">{state.currentProject.description}</div>
                 </div>
                 <div class="row">
                     <div class="col-12 h4 my-2">Built with</div>
@@ -35,86 +35,95 @@ function Project() {
                         <div class="row mb-2">
                             <div class="col-12">
                                 <ul class="list-group list-group-horizontal-lg justify-content-md-center">
-                                    {{ #each stack }}
-                                    <li class="list-group-item">{{ this}}</li>
-                                    {{/ each}}
+                                    {state.currentProject.stack.map((stack, i) => {
+                                        return (
+                                            <li key={i} class="list-group-item">{stack}</li>
+                                        )
+
+                                    })}
 
                                 </ul>
                             </div>
                         </div>
 
-                        <hr class="mx-4">
+                        <hr class="mx-4" />
 
-                            <div class="row">
-
-
-                                <div class="col-md-4">
-                                    <div class="list-group" id="list-tab" role="tablist">
-
-                                        {{ #each stack2 }}
-
-                                        {{ #is @index 0}}
-                                        <a class="list-group-item list-group-item-action active" id={{ this}} data-toggle="list"
-                                            href="#list-{{this}}" role="tab" aria-controls="{{this}}">{{ this}}</a>
-                                        {{/ is}}
+                        <div class="row">
 
 
-                                        {{ #isnt @index 0}}
-                                        <a class="list-group-item list-group-item-action" id={{ this}} data-toggle="list"
-                                            href="#list-{{this}}" role="tab" aria-controls="{{this}}">{{ this}}</a>
-                                        {{/ isnt}}
+                            <div class="col-md-4">
+                                <div class="list-group" id="list-tab" role="tablist">
 
-                                        {{/ each}}
+                                    {state.currentProject.stack2.map((stack, i) => {
 
-                                    </div>
+                                        if (i === 0) {
+                                            return (
+                                                <a class="list-group-item list-group-item-action active" id={stack} data-toggle="list"
+                                                    href={`#list-${stack}`} role="tab" aria-controls={stack}>{stack}</a>
+                                            )
+                                        }
+                                        else {
+                                            return (
+                                                <a class="list-group-item list-group-item-action" id={stack} data-toggle="list"
+                                                    href={`#list-${stack}`} role="tab" aria-controls={stack}>{stack}</a>
+                                            )
+                                        }
+
+                                    })}
+
                                 </div>
+                            </div>
 
 
-                                <div class="col-md-8">
-                                    <div class="tab-content" id="nav-tabContent">
+                            <div class="col-md-8">
+                                <div class="tab-content" id="nav-tabContent">
 
-                                        {{ #each stack2 }}
 
-                                        {{ #is @index 0}}
-                                        <img class="tab-pane fade show active codeSnippet" id="list-{{this}}" role="tabpanel"
-                                            aria-labelledby={{ this}} src="{{../snippetLink}}{{this}}.jpg" alt="Code Snippet">
-                                            {{/ is}}
+                                    {state.currentProject.stack2.map((stack, i) => {
 
-                                            {{ #isnt @index 0}}
-                                            <img class=" tab-pane fade codeSnippet" id="list-{{this}}" role="tabpanel" aria-labelledby={{ this}}
-                                                src="{{../snippetLink}}{{this}}.jpg" alt="Code Snippet">
-                                                {{/ isnt}}
+                                        if (i === 0) {
+                                            return (
+                                                <img class="tab-pane fade show active codeSnippet" id={`#list-${stack}`} role="tabpanel"
+                                                    aria-labelledby={stack} src={`${state.currentProject.snippetLink}${stack}.jpg`} alt="Code Snippet" />
+                                            )
+                                        }
+                                        else {
+                                            return (
+                                                <img class=" tab-pane fade codeSnippet" id={`#list-${stack}`} role="tabpanel" aria-labelledby={stack}
+                                                    src={`${state.currentProject.snippetLink}${stack}.jpg`} alt="Code Snippet" />
+                                            )
+                                        }
 
-                                                {{/ each}}
+                                    })}
 
+                                </div>
+                            </div>
                         </div>
+
                     </div>
-                                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 h4 my-2">Links</div>
+                    <a class="col-12 link" style="text-decoration: none;" href={state.currentProject.repo} target="_blank"><i
+                        class="fab fa-github-alt"></i> {state.currentProject.repo}</a>
+                    <a class="col-12 link" style="text-decoration: none;" href={state.currentProject.deployed} target="_blank"><i
+                        class="far fa-play-circle"></i> {state.currentProject.deployed}</a>
 
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-12 h4 my-2">Links</div>
-                                <a class="col-12 link" style="text-decoration: none;" href={{ repo }} target="_blank"><i
-                                    class="fab fa-github-alt"></i> {{ repo }}</a>
-                                <a class="col-12 link" style="text-decoration: none;" href={{ deployed }} target="_blank"><i
-                                    class="far fa-play-circle"></i> {{ deployed }}</a>
+                </div>
+                <div class="row">
+                    <img class="col-12 mt-3 py-1" src={state.currentProject.src} alt="Project Gif" />
+                </div>
 
-                            </div>
-                            <div class="row">
-                                <img class="col-12 mt-3 py-1" src={{ src }} alt="Project Gif">
+                {/* <!-- end content --> */}
+
+            </div>
+
+            {/* <!-- end grid --> */}
+
         </div>
 
-                                {/* <!-- end content --> */}
-
-                            </div>
-
-                            {/* <!-- end grid --> */}
-
-</div>
-
-                        )
+    )
 
 }
 
-                        export default Project;
+export default Project;
--- src/utils/GlobalState.js
+++ src/utils/GlobalState.js
@@ -18,14 +18,125 @@ const reducer = (state, action) => {
     }
 };
 
-
 const StoreProvider = ({ value = [], ...props }) => {
     const [state, dispatch] = useReducer(reducer, {
-        project: ''
+        currentProject: '',
+        projectDisplay: 'none',
+        projects: {
+            'artiste': {
+                projectDisplay: 'block',
+                project: req.params.project,
+                name: "Artiste Affame",
+                description: "Full-stack MERN application allows users to immerse themselves in the life of a renaissance artist. Users can play games, draw and share their art.",
+                stack: ["React", "Node", "HTML", "CSS", "Javascript"],
+                stack2: ["MongoDB", "Mongoose", "Express", "Axios", "Materialize"],
+                snippetLink: "assets/images/artiste/",
+                repo: "https://github.com/kqarlos/artiste-affame",
+                deployed: "https://artiste-affame.herokuapp.com/",
+                src: "assets/images/artiste/artiste-live.gif"
+            },
+            'electionYear': {
+                projectDisplay: 'block',
+                project: req.params.project,
+                name: "Election Year",
+                description: "Full-stack application that allows to create an account and search for their representatives.",
+                stack: ["MySQL", "Express", "Node", "Bootstrap", "Axios", "HMTL", "CSS", "Javascript"],
+                stack2: ["Sequelize", "Handlebars", "Charts"],
+                snippetLink: "assets/images/election/",
+                repo: "https://github.com/kqarlos/election-year",
+                deployed: "https://electionyear2020.herokuapp.com/",
+                src: "assets/images/election/election-year-live.gif"
+            },
+            'memoryGame': {
+                projectDisplay: 'block',
+                project: req.params.project,
+                name: "Memory Game",
+                description: "Choose a game theme and start playing. Only click each image once. Click twice and it's game over",
+                stack: ["React", "Node", "Bootstrap", "Axios", "HTML", "CSS", "Javascript"],
+                stack2: ["React-Routing", "React-State"],
+                snippetLink: "assets/images/memory/",
+                repo: "https://github.com/kqarlos/memory-game",
+                deployed: "https://kqarlos.github.io/memory-game/",
+                src: "assets/images/memory/memory-game-live.gif"
+            },
+            'noteTaker': {
+                projectDisplay: 'block',
+                project: req.params.project,
+                name: "Note Taker",
+                description: "Real-time note taking application. Take notes, update them, and delete them with this note-taker.",
+                stack: ["Node", "Express", "Bootstrap", "HTML", "CSS"],
+                stack2: ["Javascript"],
+                snippetLink: "assets/images/note/",
+                repo: "https://github.com/kqarlos/note-taker",
+                deployed: "https://note-taker2020.herokuapp.com/",
+                src: "assets/images/note/note-taker-live.gif"
+            },
+            'googleLibrary': {
+                projectDisplay: 'block',
+                project: req.params.project,
+                name: "Google Library",
+                description: "Search through Google Books API and keep a list of your favorite books.",
+                stack: ["MongoDB", "Express", "React", "Node", "Bootstrap", "Axios", "Mongoose"],
+                stack2: ["React-Reducer"],
+                snippetLink: "assets/images/google/",
+                repo: "https://github.com/kqarlos/google-library",
+                deployed: "https://google-library-mern.herokuapp.com/",
+                src: "assets/images/google/google-library-live.gif"
+            },
+            'passwordGenerator': {
+                projectDisplay: 'block',
+                project: req.params.project,
+                name: "Password Generator",
+                description: "Generates a random password based on user preferences and copies it to the clipboard.",
+                stack: ["HTML", "CSS", "Javascript", "Bootstrap"],
+                repo: "https://github.com/kqarlos/password-generator",
+                deployed: "https://kqarlos.github.io/password-generator/",
+                src: "assets/images/password-generator.png"
+            },
+            'dayPlanner': {
+                projectDisplay: 'block',
+                project: req.params.project,
+                name: "Day Planner",
+                description: "A planner for day activities that saves information to local storage",
+                stack: ["HTML", "CSS", "Javascript", "Moment.js", "Bootstrap"],
+                repo: "https://github.com/kqarlos/day-planner",
+                deployed: "https://kqarlos.github.io/day-planner/",
+                src: "assets/images/day-planner.png"
+            },
+            'weatherDashboard': {
+                projectDisplay: 'block',
+                project: req.params.project,
+                name: "Weather Dashboard",
+                description: "Displays current weather and forecast for a searched city.",
+                stack: ["HTML", "CSS", "Javascript", "Moment.js", "Bootstrap"],
+                repo: "https://github.com/kqarlos/weather-dashboard",
+                deployed: "https://kqarlos.github.io/weather-dashboard/",
+                src: "assets/images/weather-live.gif"
+            },
+            'dinnerParty': {
+                projectDisplay: 'block',
+                project: req.params.project,
+                name: "Dinner Party",
+                description: "Application that recommends a dish and a cocktail based on a protein choice",
+                stack: ["HTML", "CSS", "Javascript", "Materialize", "Bootstrap"],
+                repo: "https://github.com/kqarlos/dinner-party",
+                deployed: "https://kqarlos.github.io/dinner-party/",
+                src: "assets/images/dinner-party-live.gif"
+            },
+            'shoppingBuddy': {
+                projectDisplay: 'block',
+                project: req.params.project,
+                name: "Shopping Buddy",
+                description: "Manage your shopping list on the go",
+                stack: ["HTML", "CSS", "Javascript", "Express", "Node", "Handlebars", "MySQL"],
+                repo: "https://github.com/kqarlos/shopping-buddy",
+                deployed: "https://shopping-buddy2020.herokuapp.com/",
+                src: "assets/images//shopping/shopping-buddy.gif"
+            },
+        }
     });
     return <Provider value={[state, dispatch]} {...props} />;
 };
-
 
 const useStoreContext = () => {
     return useContext(StoreContext);
