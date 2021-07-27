a0fc94efcb9a89743dd1da484734e03fe38027f6
kqarlos <kqarlos@hotmail.com>
2021-07-26 17:56:19 -0700

    All project cards are displayed using the projectCard component

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,7 +4,7 @@ import artisteImg from "../../assets/images/artiste/artiste.png";
 import electionYearImg from "../../assets/images/election/election-year.png";
 import googleLibraryImg from "../../assets/images/google/google-library.png";
 import memoryGameImg from "../../assets/images/memory/memory-game.png";
-import codeQuizImg from "../../assets/images/note/note-taker.png";
+import noteTakerImg from "../../assets/images/note/note-taker.png";
 import dinnerPartyImg from "../../assets/images/dinner-party.png";
 import shoppingBuddyImg from "../../assets/images/shopping/shopping-buddy.png";
 import passGenImg from "../../assets/images/password-generator.png";
@@ -37,8 +37,6 @@ function Portfolio() {
 
         <div id="portfolio">
 
-            {/* <!-- start grid 2--> */}
-
             <div className=" p-4 bg-white border portfolioContent">
 
                 {/* <!-- first row / title --> */}
@@ -50,119 +48,55 @@ function Portfolio() {
 
                 {/* <!-- second row / content --> */}
                 <div className="row">
-
                     <div className="col-12 col-xl-4 my-1">
-
                         <ProjectCard imgLink={artisteImg} imgAlt="Drawing on a canvas" handleClick={handleClick}
-                            title="Artiste Affame" name="artiste" description="Live the life of a starving artist in Artiste Affame!" />
-
+                            name="artiste" title="Artiste Affame" description="Live the life of a starving artist in Artiste Affame!" />
                     </div>
                     <div className="col-12 col-xl-4 my-1 px-1">
-
-                        <div className="card text-center">
-                            <img className="card-img-top" src={electionYearImg} alt="Campaign contributions chart" />
-                            <div className="card-body projectCard">
-                                <a href="#project" onClick={() => handleClick('electionYear')} className="btn projectBtn text-white bg-info contentText w-100 rightCorners">Election
-                                    Year</a>
-                                <p className="card-text m-2">Political app to keep track of your federal representatives</p>
-                            </div>
-                        </div>
+                        <ProjectCard imgLink={electionYearImg} imgAlt="Campaign contributions chart" handleClick={handleClick}
+                            name="electionYear" title="Election Year" description="Political app to keep track of your federal representatives" />
                     </div>
                     <div className="col-12 col-xl-4 my-1">
-
-                        <div className="card  text-center">
-                            <img className="card-img-top" src={googleLibraryImg} alt="Google books search result" />
-                            <div className="card-body projectCard">
-                                <a href="#project" onClick={() => handleClick('googleLibrary')} className="btn projectBtn text-white bg-info contentText w-100 rightCorners">Google
-                                    Book
-                                    Library</a>
-                                <p className="card-text m-2">Search through Google Books API and keep a list of your favorite books.
-                                </p>
-                            </div>
-                        </div>
+                        <ProjectCard imgLink={googleLibraryImg} imgAlt="Google books search result" handleClick={handleClick}
+                            name="googleLibrary" title="Google Book Library" description="Search through Google Books API and keep a list of your favorite books" />
                     </div>
                 </div>
 
                 {/* <!-- third row / content --> */}
                 <div className="row">
-
                     <div className="col-12 col-lg-6  my-1">
-                        <div className="card text-center">
-                            <img className="card-img-top" src={memoryGameImg} alt="Memory game" />
-                            <div className="card-body projectCard">
-                                <a href="#project" onClick={() => handleClick('memoryGame')} className="btn projectBtn text-white bg-info contentText w-100 rightCorners">Memory
-                                    Game</a>
-                                <p className="card-text m-2">Card matching game to test your memory skills</p>
-                            </div>
-                        </div>
+                        <ProjectCard imgLink={memoryGameImg} imgAlt="Memory Game" handleClick={handleClick}
+                            name="memoryGame" title="Memory Game" description="Card matching game to test your memory skills" />
                     </div>
-
                     <div className="col-12 col-lg-6  my-1">
-                        <div className="card text-center">
-                            <img className="card-img-top" src={codeQuizImg} alt="Note taker" />
-                            <div className="card-body projectCard">
-                                <a href="#project" onClick={() => handleClick('noteTaker')} className="btn projectBtn text-white bg-info contentText w-100 rightCorners">Note Taker</a>
-                                <p className="card-text m-2">Real-time note taking application</p>
-                            </div>
-                        </div>
+                        <ProjectCard imgLink={noteTakerImg} imgAlt="Note taker" handleClick={handleClick}
+                            name="noteTaker" title="Note Taker" description="Real-time note taking application" />
                     </div>
-
                     <div className="col-12 col-lg-6  my-1">
-                        <div className="card text-center">
-                            <img className="card-img-top" src={dinnerPartyImg} alt="Dinner Party" />
-                            <div className="card-body projectCard">
-                                <a href="#project" onClick={() => handleClick('dinnerParty')} className="btn projectBtn text-white bg-info contentText w-100 rightCorners">Dinner Party</a>
-                                <p className="card-text m-2">Let our API digging recommend your next meal</p>
-                            </div>
-                        </div>
+                        <ProjectCard imgLink={dinnerPartyImg} imgAlt="Dinner Party" handleClick={handleClick}
+                            name="dinnerParty" title="Dinner Party" description="Let our API digging recommend your next meal" />
                     </div>
-
                     <div className="col-12 col-lg-6 my-1">
-                        <div className="card text-center">
-                            <img className="card-img-top" src={shoppingBuddyImg} alt="Shopping Buddy" />
-                            <div className="card-body projectCard">
-                                <a href="#project" onClick={() => handleClick('shoppingBuddy')} className="btn projectBtn text-white bg-info contentText w-100 rightCorners">Shopping Buddy</a>
-                                <p className="card-text m-2">Create a shopping list. Add and delete items from your list on the go!
-                                </p>
-                            </div>
-                        </div>
+                        <ProjectCard imgLink={shoppingBuddyImg} imgAlt="Shopping Buddy" handleClick={handleClick}
+                            name="shoppingBuddy" title="Shopping Buddy" description="Create a shopping list. Add and delete items from your list on the go!" />
                     </div>
-
                 </div>
 
                 {/* <!-- fourth row / content --> */}
                 <div className="row">
-
                     <div className="col-12 col-lg-4 col-xl-4 my-2">
-                        <div className="card text-center">
-                            <img className="card-img-top" src={passGenImg} alt="Password Generator" />
-                            <div className="card-body projectCard">
-                                <a href="#project" onClick={() => handleClick('passwordGenerator')} className="btn projectBtn text-white bg-info contentText w-100 rightCorners">Password
-                                    Generator</a>
-                                <p className="card-text m-2">Generate a password according to your specifications</p>
-                            </div>
-                        </div>
+                        <ProjectCard imgLink={passGenImg} imgAlt="Password Generator" handleClick={handleClick}
+                            name="passwordGenerator" title="Password Generator" description="Generate a password according to your specifications" />
                     </div>
 
                     <div className="col-12 col-lg-4 col-xl-4 my-2 px-1">
-                        <div className="card text-center">
-                            <img className="card-img-top" src={dayPlannerImg} alt="Day Planner" />
-                            <div className="card-body projectCard">
-                                <a href="#project" onClick={() => handleClick('dayPlanner')} className="btn projectBtn text-white bg-info contentText w-100 rightCorners">Day Planner</a>
-                                <p className="card-text m-2">Plan your day in real time</p>
-                            </div>
-                        </div>
+                        <ProjectCard imgLink={dayPlannerImg} imgAlt="Day Planner" handleClick={handleClick}
+                            name="dayPlanner" title="Day Planner" description="Plan your day in real time" />
                     </div>
 
                     <div className="col-12 col-lg-4 col-xl-4 my-2">
-                        <div className="card text-center">
-                            <img className="card-img-top" src={weatherDashImg} alt="Weather Dashboard" />
-                            <div className="card-body projectCard">
-                                <a href="#project" onClick={() => handleClick('weatherDashboard')} className="btn projectBtn text-white bg-info contentText w-100 rightCorners">Weather
-                                    Dashboard</a>
-                                <p className="card-text m-2">Check your local weather and keep track of your favorite cities</p>
-                            </div>
-                        </div>
+                        <ProjectCard imgLink={weatherDashImg} imgAlt="Weather Dashboard" handleClick={handleClick}
+                            name="weatherDashboard" title="Weather Dashboard" description="Check your local weather and keep track of your favorite cities" />
                     </div>
 
                 </div>
@@ -170,8 +104,6 @@ function Portfolio() {
                 {/* <!-- end content --> */}
 
             </div>
-
-            {/* <!-- end grid 2--> */}
 
         </div>
     )
